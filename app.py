06e322f703254bfdc52fb91e9f1819f7a0ea8988
wfg.langfang <mikewang0814@gmail.com>
2020-09-24 22:27:45 +0800

    initial code

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from waitress import serve
-from OrderAPI.Resource.user_resource import UserResource
+from Resource.user_resource import UserResource
 
 app = Flask(__name__)
 api = Api(app)
@@ -17,3 +17,6 @@ api.add_resource(UserResource, '/User', '/User/<string:OpCode>')
 
 if __name__ == '__main__':
     serve(app, host="0.0.0.0", port=6000)
+
+
+# 'app.py', 'Model/user.py','Resource/user_resource.py','Service/user_dao.py','Service/user_service.py'
