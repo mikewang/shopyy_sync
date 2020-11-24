4cf244ae5ed27bca4a60ae67bde7ee750476ab7d
mikewang <mikewang0814@gmail.com>
2020-11-24 10:25:27 +0800

    修改 make mace 文件

--- hotel_sync/execute_mace.py
+++ hotel_sync/execute_mace.py
@@ -23,7 +23,7 @@ def get_agent_filelist():
 def open_agent_file(file):
     if file.split('.')[2] == 'inp':
         fegms_sh = os.path.normpath(os.path.join(os.curdir, "fegms.sh"))
-        gms_out_filename = subprocess.run([fegms_sh, file], stdout=subprocess.PIPE)
+        gms_out_filename = subprocess.run(["./fegms.sh", file], stdout=subprocess.PIPE)
         # 同时生成 dat文件，用于下一个。
         gms_out_filename = file + ".out"
         return gms_out_filename
@@ -35,8 +35,10 @@ def open_agent_file(file):
 def get_q(file_name):
     file_name_out = file_name + ".out"
     file_name_q = file_name + ".q"
+    print("get_q", file_name_out, file_name_q)
     extract_gradmatrix_sh = os.path.normpath(os.path.join(os.curdir, "extract_gradmatrix.sh"))
-    returncode = subprocess.run([extract_gradmatrix_sh, file_name_out, file_name_q])
+    print("extract_gradmatrix.sh filepath = ", extract_gradmatrix_sh)
+    returncode = subprocess.run(["./extract_gradmatrix.sh", file_name_out, file_name_q])
     if returncode.returncode != 0:
         print("Error extracting gradient from GAMESS output.  Agent in question:")
         print(file_name)
@@ -62,7 +64,9 @@ def get_q(file_name):
 
 def make_mace(file_first, gamma, tolerance):
     start_time = time.time()
-    # get q from first file
+    # create .out file from .inp file
+    open_agent_file(file_first)
+    # create .q from first .out file
     q1 = get_q(file_first)
     done = 0
     iterationcount = 0
@@ -74,13 +78,16 @@ def make_mace(file_first, gamma, tolerance):
         iter_time_start = time.time()
         # get q from second file
         extract_dat_to_inp_sh = os.path.normpath(os.path.join(os.curdir, "extract_dat_to_inp.sh"))
-        subprocess.run([extract_dat_to_inp_sh])
+        subprocess.run(["./extract_dat_to_inp.sh"])
         file_num = int(current_file.split('.')[1]) + 1
         file_next = current_file.split('.')[0] + "." + str(file_num) + ".inp"
         if not os.path.isfile(file_next):
             print("Error file is not existed .", file_next)
             break
             return
+        # create .out  next file from .inp next file
+        open_agent_file(file_next)
+        # create .q from .out next file
         q2 = get_q(file_next)
         q_out, convergence_flag = compute_MACE_step(q1, q2, gamma, tolerance)
 
