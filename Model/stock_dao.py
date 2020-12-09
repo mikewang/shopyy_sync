cc157c650826c973dbcdf0d2e9c653914fc35842
mikewang <mikewang0814@gmail.com>
2020-12-09 17:13:32 +0800

    修改 stock

--- Model/stock_dao.py
+++ Model/stock_dao.py
@@ -133,14 +133,14 @@ class StockDao(object):
                             "c.ModuleID,CONVERT(varchar, c.FileDate, 120 ) as FileDate,c.ThumbImage,b.[其它.供应商名称]," \
                             "b.[其它.允采购量],b.[其它.应采购价],b.[其它.商品品牌], " \
                             "coalesce(g.ordernum,0) as ordernum, coalesce(h.id,0) as priceEnquiredID  " \
-                            "FROM [csidbo].[Stock_Product_Info] as a " \
-                            "join  csidbo.FTPart_Stock_Product_Property_1 as b on a.StockProductID=b.MainID " \
-                            "join csidbo.Product_Image as c on b._ImageID=c.ProductImageID " \
-                            "join csidbo.stock_info d on d.ID=a.StockID " \
-                            "join csidbo.[FTPart_Stock_Property_1] e on e.[MainID] = d.ID " \
-                            "left join csidbo.[Stock_Product_Info_Desc] f on a.StockProductID=f.StockProductID " \
-                            "left join (select [StockProductID], sum([OrderNum]*[OrderStat]) as ordernum from [csidbo].[Stock_Product_Order_App] group by [StockProductID]) g on a.StockProductID=g.StockProductID  " \
-                            "left join (select max(id) as id, StockProductID from csidbo.[Stock_Product_EnquiryPrice_App] group by StockProductID ) h on  a.StockProductID=h.StockProductID "
+                            "FROM [Stock_Product_Info] as a " \
+                            "join FTPart_Stock_Product_Property_1 as b on a.StockProductID=b.MainID " \
+                            "join Product_Image as c on b._ImageID=c.ProductImageID " \
+                            "join stock_info d on d.ID=a.StockID " \
+                            "join [FTPart_Stock_Property_1] e on e.[MainID] = d.ID " \
+                            "left join [Stock_Product_Info_Desc] f on a.StockProductID=f.StockProductID " \
+                            "left join (select [StockProductID], sum([OrderNum]*[OrderStat]) as ordernum from [Stock_Product_Order_App] group by [StockProductID]) g on a.StockProductID=g.StockProductID  " \
+                            "left join (select max(id) as id, StockProductID from [Stock_Product_EnquiryPrice_App] group by StockProductID ) h on  a.StockProductID=h.StockProductID "
             v_sql = v_sql + "  where b.[其它.允采购量] > coalesce(g.ordernum,0) "
 
             filter_brand = filter_stock["brand"]
